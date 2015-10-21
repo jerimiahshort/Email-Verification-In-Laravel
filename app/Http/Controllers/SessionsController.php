ef78f8bb772ffecda96a6fe3af26efaf20ace543
jerimiah.short <jerimiahshort@users.noreply.github.com>
2015-10-21 07:40:47 -0400

    Updated middleware to allow logout
    Middleware was interfering with the ability to hit the logout method. Adding the except rules allows this to function as expected.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,7 +11,7 @@ class SessionsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('guest', ['except' => 'logout']);
     }
 
     /**
